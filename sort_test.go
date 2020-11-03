4f60b9ec1fb3f3ddf90740cd313f9eca383dbac7
jdao25 <johnnydao4@gmail.com>
2020-11-02 16:31:17 -0800

    I removed QuickSort test

--- sort_test.go
+++ sort_test.go
@@ -16,8 +16,8 @@ type TestCase struct {
 
 // Global variables
 var (
-	numTest int = 5                         // Number of test cases to run for each
-	size    int = 20                        // Set the size of the list to generate
+	numTest int = 1000                  // Number of test cases to run for each
+	size    int = 20                    // Set the size of the list to generate
 	list        = make([]TestCase, numTest) // List to hold all test cases
 )
 
@@ -116,96 +116,6 @@ func TestMergeSortGo(t *testing.T) {
 	}
 }
 
-func TestQuickSort(t *testing.T) {
-	/*
-	   Testing the our quick sort with the standard sorting algorithm
-
-	   Our QuickSort function should give us the same result as the standard
-	   sort function.
-
-	   Example:
-	       if QuickSort(myList) == sort.Ints(myList)
-	*/
-
-	rand.Seed(time.Now().UnixNano())
-
-	for i := 0; i < numTest; i++ {
-		rList := generateRandomList(size) // A randomly generated list
-
-		// temp is the sorted rList
-		temp := make([]int, size)
-		for i := 0; i < size; i++ {
-			temp[i] = rList[i]
-		}
-
-		list[i].Unsorted = rList // Value is the unsorted list
-		sort.Ints(temp)          // Use built-in sorting algorithm to sort
-		list[i].Sorted = temp    // Assign Sorted to temp
-	}
-
-	// Iterate through the test cases
-	for _, tc := range list {
-		t.Run(fmt.Sprintf("%v", tc.Unsorted), func(t *testing.T) {
-			// Use our QuickSort function we implemented
-			QuickSort(&tc.Unsorted)
-
-			/*
-			   tc.Unsorted = actual sorted result
-			   tc.Sorted = expected sorted result
-			   Comparing if actual = expected, if so continue, else error
-			*/
-			if !reflect.DeepEqual(tc.Unsorted, tc.Sorted) {
-				t.Fatalf("Not Equal - %v != %v", tc.Unsorted, tc.Sorted)
-			}
-		})
-	}
-}
-
-func TestQuickSortGo(t *testing.T) {
-	/*
-	   Testing the our quick sort (w/ goroutine) with the standard sorting
-	   algorithm.
-
-	   Our QuickSort function (w/ goroutine) should give us the same result
-	   as the standard sort function.
-
-	   Example:
-	       if QuickSortGo(myList) == sort.Ints(myList)
-	*/
-
-	rand.Seed(time.Now().UnixNano())
-
-	for i := 0; i < numTest; i++ {
-		rList := generateRandomList(size) // A randomly generated list
-
-		// temp is the sorted rList
-		temp := make([]int, size)
-		for i := 0; i < size; i++ {
-			temp[i] = rList[i]
-		}
-
-		list[i].Unsorted = rList // Value is the unsorted list
-		sort.Ints(temp)          // Use built-in sorting algorithm to sort
-		list[i].Sorted = temp    // Assign Sorted to temp
-	}
-
-	// Iterate through the test cases
-	for _, tc := range list {
-		t.Run(fmt.Sprintf("%v", tc.Unsorted), func(t *testing.T) {
-			// Use our QuickSortGo function we implemented
-			QuickSortGo(&tc.Unsorted)
-
-			/*
-			   tc.Unsorted = actual sorted result
-			   tc.Sorted = expected sorted result
-			   Comparing if actual = expected, if so continue, else error
-			*/
-			if !reflect.DeepEqual(tc.Unsorted, tc.Sorted) {
-				t.Fatalf("Not Equal - %v != %v", tc.Unsorted, tc.Sorted)
-			}
-		})
-	}
-}
 func TestInsertionSort(t *testing.T) {
 	/*
 	   Testing our insertion sort with the standard sorting
@@ -251,3 +161,95 @@ func TestInsertionSort(t *testing.T) {
 		})
 	}
 }
+
+//
+// func TestQuickSort(t *testing.T) {
+// 	/*
+// 	   Testing the our quick sort with the standard sorting algorithm
+//
+// 	   Our QuickSort function should give us the same result as the standard
+// 	   sort function.
+//
+// 	   Example:
+// 	       if QuickSort(myList) == sort.Ints(myList)
+// 	*/
+//
+// 	rand.Seed(time.Now().UnixNano())
+//
+// 	for i := 0; i < numTest; i++ {
+// 		rList := generateRandomList(size) // A randomly generated list
+//
+// 		// temp is the sorted rList
+// 		temp := make([]int, size)
+// 		for i := 0; i < size; i++ {
+// 			temp[i] = rList[i]
+// 		}
+//
+// 		list[i].Unsorted = rList // Value is the unsorted list
+// 		sort.Ints(temp)          // Use built-in sorting algorithm to sort
+// 		list[i].Sorted = temp    // Assign Sorted to temp
+// 	}
+//
+// 	// Iterate through the test cases
+// 	for _, tc := range list {
+// 		t.Run(fmt.Sprintf("%v", tc.Unsorted), func(t *testing.T) {
+// 			// Use our QuickSort function we implemented
+// 			QuickSort(&tc.Unsorted)
+//
+// 			/*
+// 			   tc.Unsorted = actual sorted result
+// 			   tc.Sorted = expected sorted result
+// 			   Comparing if actual = expected, if so continue, else error
+// 			*/
+// 			if !reflect.DeepEqual(tc.Unsorted, tc.Sorted) {
+// 				t.Fatalf("Not Equal - %v != %v", tc.Unsorted, tc.Sorted)
+// 			}
+// 		})
+// 	}
+// }
+//
+// func TestQuickSortGo(t *testing.T) {
+// 	/*
+// 	   Testing the our quick sort (w/ goroutine) with the standard sorting
+// 	   algorithm.
+//
+// 	   Our QuickSort function (w/ goroutine) should give us the same result
+// 	   as the standard sort function.
+//
+// 	   Example:
+// 	       if QuickSortGo(myList) == sort.Ints(myList)
+// 	*/
+//
+// 	rand.Seed(time.Now().UnixNano())
+//
+// 	for i := 0; i < numTest; i++ {
+// 		rList := generateRandomList(size) // A randomly generated list
+//
+// 		// temp is the sorted rList
+// 		temp := make([]int, size)
+// 		for i := 0; i < size; i++ {
+// 			temp[i] = rList[i]
+// 		}
+//
+// 		list[i].Unsorted = rList // Value is the unsorted list
+// 		sort.Ints(temp)          // Use built-in sorting algorithm to sort
+// 		list[i].Sorted = temp    // Assign Sorted to temp
+// 	}
+//
+// 	// Iterate through the test cases
+// 	for _, tc := range list {
+// 		t.Run(fmt.Sprintf("%v", tc.Unsorted), func(t *testing.T) {
+// 			// Use our QuickSortGo function we implemented
+// 			QuickSortGo(&tc.Unsorted)
+//
+// 			/*
+// 			   tc.Unsorted = actual sorted result
+// 			   tc.Sorted = expected sorted result
+// 			   Comparing if actual = expected, if so continue, else error
+// 			*/
+// 			if !reflect.DeepEqual(tc.Unsorted, tc.Sorted) {
+// 				t.Fatalf("Not Equal - %v != %v", tc.Unsorted, tc.Sorted)
+// 			}
+// 		})
+// 	}
+// }
